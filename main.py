fa270aa9a6bec9539af1e78d0462aa06e8ea3164
Malcolm White <mwhite2@rrc.ca>
2023-11-10 18:27:47 -0600

    Complete overhaul of password generator logic.

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-
-
 # Setup
 
 import os
@@ -9,62 +7,55 @@ from time import sleep
 
 
 
-valid_decisions = ('y', 'n')
-password_length = 12
+password_length = 0
 interface_active = True
 element_data = ['a','b','c','d','e','f','g','h','i','j','k','l','m','o','p',
-                'q','r','s','t','u','v','w','x','y','z']
-
+                'q','r','s','t','u','v','w','x','y','z','A','B','C','D','E',
+                'F','G''H','I','J','K','L','M','N','O','P','Q','R','S','T',
+                'U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9',
+                '!','@','#','$']
+valid_choices = ('y','n')
 
 # Interface
 
 while interface_active:
-    slowtype('PASSWORD GENERATOR\n', 0.05)
-    sleep(0.5)
-    slowtype('BY MALCOLM WHITE\n', 0.05)
-    sleep(0.5)
-    decision = input('Would you like to generate a password? Y/N: ')
-    decision.lower()
 
-    if decision not in valid_decisions:
-        print('Invalid Selection.')
-    
-    elif decision == 'y':
-        letter_one = random.choice(element_data)
-        letter_one.upper()
-        letter_two = random.choice(element_data)
-        letter_three = random.choice(element_data)
-        letter_four = random.choice(element_data)
-        letter_four.upper()
-        letter_five = random.choice(element_data)
-        letter_six = random.choice(element_data)
-        letter_six.upper()
-        letter_seven = random.choice(element_data)
-        letter_eight = random.choice(element_data)
-        letter_nine = random.choice(element_data)
-        letter_ten = random.choice(element_data)
-        letter_eleven = random.choice(element_data)
-        letter_twelve = random.choice(element_data)
-        
-        passnum = random.randint(1000,9999)
-        str(passnum)
+    slowtype('PASSWORD GENERATOR\n', 0.02)
+    sleep(0.5)
+    slowtype('BY MALCOLM WHITE\n', 0.02)
+    sleep(0.5)
+    keep_going = True
 
-        password = (letter_one +
-                    letter_two +
-                    letter_three +
-                    letter_four +
-                    letter_five +
-                    letter_six +
-                    letter_seven +
-                    letter_eight +
-                    letter_nine +
-                    letter_ten +
-                    letter_eleven +
-                    letter_twelve)
+    while keep_going:
+        decision = input('Please enter the desired password length: ')
+        password_length = decision
+        valid = decision.isnumeric()
+
+        try:
+            if valid != True:
+                raise ValueError
+            else:
+                password_length = int(password_length)
+                password = ''
+                for i in range(0, password_length):
+                    random_char = random.choice(element_data)
+                    password += random_char
                     
+            slowtype(f'Here is your randomly generated password: {password} \n',0.02)
+            try_again = input('Would you like to generate another password? Y/N: ')
+            try_again = try_again.lower()
+            if try_again == 'y':
+                continue
+            elif try_again == 'n':
+                os.system('cls')
+                keep_going = False
+        except ValueError:
+            print('Please enter a whole number.')
+            
         
-        print(password)
+        
+            
+
         
 
 
-     
